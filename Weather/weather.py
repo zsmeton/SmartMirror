cf180cf527886e3b087e23412a9fc9964002485a
zsmeton <zsmeton@yahoo.com>
2018-12-09 18:47:53 -0700

    Added basic weather code

---
+++ Weather/weather.py
@@ -0,0 +1,22 @@
+import pyowm
+
+# Open Weather Map API
+# My API Key: 95e49145dd2f7199e468eceb3e9acca9
+
+
+# Testing/example
+if __name__ == "__main__":
+    # Tuple to hold the current location
+    current_location = {'lat': 37.09024, 'lon': -95.712891}
+    # Create OWM instance using my api key
+    owm = pyowm.OWM(API_key='95e49145dd2f7199e468eceb3e9acca9')
+    # Create observation instance for Current Location
+    observation = owm.weather_at_coords(lat=current_location['lat'], lon=current_location['lon'])
+    # Get the weather data from that observation
+    current_weather = observation.get_weather()
+    print(current_weather.get_wind())
+    print(current_weather.get_rain())
+    print(current_weather.get_snow())
+    print(current_weather.get_clouds())
+    print(current_weather.get_temperature(unit='fahrenheit'))
+    print(current_weather.get_clouds())
