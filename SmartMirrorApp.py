ca6e32411c24fe0e71cfa0365782d030cb72f175
zsmeton <zsmeton@yahoo.com>
2019-01-14 21:32:16 -0700

    Added loading screen widget

--- SmartMirrorApp.py
+++ SmartMirrorApp.py
@@ -1,20 +1,47 @@
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.config import Config
-Config.set('graphics', 'fullscreen', 'auto')
-
-
+from kivy.properties import NumericProperty, ObjectProperty, ReferenceListProperty
 from kivy.uix.widget import Widget
 
-### Set Kivy App configurations
+# Set Kivy App configurations
+Config.set('graphics', 'fullscreen', 'auto')  # Sets window to fullscreen
+
+
+# Widget that creates a small loading screen while other processes run
+class LoadingScreen(Widget):
+    starting_angle = NumericProperty(0)
+    ending_angle = NumericProperty(0)
+    size_x = NumericProperty(100)
+    size_y = NumericProperty(100)
+    size = ReferenceListProperty(size_x, size_y)
+    thickness = NumericProperty(10)
+    color_r = 0
+    color_g = 230
+    color_b = 255
+
+    def update(self, dt):
+        if self.starting_angle // 360 == 0:
+            self.starting_angle += 2
+            self.ending_angle += 1
+        elif self.ending_angle != self.starting_angle:
+            self.ending_angle += 2
+            self.ending_angle += 1
+        else:
+            self.ending_angle = 0
+            self.starting_angle = 0
 
 
 class MirrorDisplay(Widget):
-    temp = 1
+    load = ObjectProperty(None)
 
 
 class SmartMirrorApp(App):
     def build(self):
-        return MirrorDisplay()
+        display = MirrorDisplay()
+        Clock.schedule_interval(display.load.update, 1.0 / 60.0)
+
+        return display
 
 
 if __name__ == '__main__':
